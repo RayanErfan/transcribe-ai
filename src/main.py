472ec67494d95be26c369ce6630ea15e0a78a896
RayanErfan <erfanrayanipour@gmail.com>
2025-10-18 19:52:47 +0330

    Added config.py for better access and i removed unnecessary AudioRequest Model and added request Transcribe with its class

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from aiohttp import ClientSession
-from fastapi import FastAPI
+from unittest import result
+from aiohttp import ClientSession, ClientTimeout, request
+from fastapi import FastAPI, Form, HTTPException
 from typing import Optional, List, Union, Dict, Any
 from pydantic import BaseModel, Field
-
+from config import Config
 
 
 app = FastAPI()
@@ -13,8 +14,60 @@ class AudioRequest(BaseModel):
     lang_code: str = Field(..., description="The language code of the audio file ( en or fa or ar or de or es or fr or it or ja or ko or nl or pl or pt or ru or tr or zh )")
     diarization: bool = Field(..., description="Whether to enable diarization (bool)")
     accuracy: str = Field(..., description="The accuracy of the transcription (medium or high or low)")
-    referrer_url: str = Field(..., description="The referrer URL of the audio file ( /audio-to-text-converter )")
-    audio_time: int = Field(..., description="The audio time of the audio file (in seconds)")
-    file_name: str = Field(..., description="The file name of the audio file (Tiger Tiger.mp3)")
 
-# @app.post("/transcribe")
+
+
+
+class Transcriber:
+
+    """This is where the real magic happens
+    DO NOT TOUCH OR YOU WILL BE FIRED ( i always wanted to say this )"""
+
+    def __init__(self) -> None:
+
+        self.start_url: str = Config.START_URL
+        self.check_url: str = Config.CHECK_URL
+        self.timeout: int = 60
+
+
+    async def request_transcribe(
+        self,
+        path: str,
+        type: int = 1,
+        diarization: bool = False,
+        lang_code: str = "en",
+        accuracy: str = "medium",
+        
+    ) -> Dict[str, Any]:
+
+        # request params 
+        form = {
+            "path": path,
+            "type": type,
+            "diarization": diarization,
+            "lang_code": lang_code,
+            "accuracy": accuracy
+        }
+
+        #TODO; request headers
+
+        timeout = ClientTimeout(total=self.timeout)
+        async with ClientSession(timeout=timeout) as ses:
+            async with ses.post(self.start_url, params=form, headers=None) as res:
+                request_status = res.get('code', 0) == 100000 and res.get('message', "failed") == "success"
+                if res.status == 200 and request:
+                    #TODO; handle the ok res
+                    pass
+                else:
+                    res_text = res.text()
+                    raise HTTPException(res.status, f"We got an error: {res_text}")
+
+
+
+@app.post("/transcribe")
+
+#TODO 
+# Add file -> url
+
+async def transcribe(request: AudioRequest):
+    pass
